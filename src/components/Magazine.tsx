f50132f929242b9e31d329024371f9d7ac42b0ad
Dhanush Naik <dhanushlnaik@gmail.com>
2024-01-31 00:53:28 +0530

    ..

--- src/components/Magazine.tsx
+++ src/components/Magazine.tsx
@@ -1,23 +1,23 @@
-// @ts-ignore
-import * as React from "react";
-import HTMLFlipBook from "react-pageflip";
-import Image from 'next/image';
+// //@ts-ignore
+// import * as React from "react";
+// import HTMLFlipBook from "react-pageflip";
+// import Image from 'next/image';
 
-export const Magazine = () => {
-  const pageImages = Array.from({ length: 42 }, (_, index) => index + 1);
+// export const Magazine = () => {
+//   const pageImages = Array.from({ length: 42 }, (_, index) => index + 1);
 
-  return (
-    <HTMLFlipBook width={700} height={800}>
-      {pageImages.map((pageNumber) => (
-        <div key={pageNumber} className="demoPage">
-          <Image
-            src={`/magazine/mag_page-${String(pageNumber).padStart(4, '0')}.jpg`}
-            width={700}
-            height={800}
-            alt={`mag${pageNumber}`}
-          />
-        </div>
-      ))}
-    </HTMLFlipBook>
-  );
-};
+//   return (
+//     <HTMLFlipBook width={700} height={800}>
+//       {pageImages.map((pageNumber) => (
+//         <div key={pageNumber} className="demoPage">
+//           <Image
+//             src={`/magazine/mag_page-${String(pageNumber).padStart(4, '0')}.jpg`}
+//             width={700}
+//             height={800}
+//             alt={`mag${pageNumber}`}
+//           />
+//         </div>
+//       ))}
+//     </HTMLFlipBook>
+//   );
+// };
