b27398bdd83dce8b6f50df60cbbdf16e90fe6e0d
Dhanush Naik <dhanushlnaik@gmail.com>
2024-03-19 00:18:48 +0530

    ..a.

--- public/assets/events/main.py
+++ public/assets/events/main.py
@@ -2,13 +2,13 @@
 # import OS module
 import os
 # Get the list of all files and directories
-path = "D://CSI NMAMIT//csinmamit//public//assets//events"
+path = "D://CSI NMAMIT//csinmamit//public//eve"
 dir_list = os.listdir(path)
 print("Files and directories in '", path, "' :")
 # prints all files
 print(dir_list)
 li = []
 for i in dir_list:
-    a = f"/assets/events/{i}"
+    a = f"/eve/{i}"
     li.append(a)
 print(li)
