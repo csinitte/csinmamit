aca6976fe6ddae6561d41f1a0d5aff59fadb27b3
Dhanush Naik <dhanushlnaik@gmail.com>
2024-03-22 14:34:37 +0530

    next config

--- next.config.js
+++ next.config.js
@@ -1,43 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  distDir: "dist",
-  trailingSlash: true,
-  assetPrefix: "/",
-  output: "export",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 
   async redirects() {
     return [
       {
-        source: "/sign-in",
-        destination: "/api/auth/login",
+        source: '/sign-in',
+        destination: '/api/auth/login',
         permanent: true,
       },
       {
-        source: "/sign-up",
-        destination: "/api/auth/register",
+        source: '/sign-up',
+        destination: '/api/auth/register',
         permanent: true,
       },
-    ];
+
+    ]
   },
 
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
+
+  webpack: (
+    config,
+    { buildId, dev, isServer, defaultLoaders, webpack }
+  ) => {
+    config.resolve.alias.canvas = false
+    config.resolve.alias.encoding = false
+    return config
   },
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "firebasestorage.googleapis.com",
-      "github.com",
-    ],
-  },
-};
+    domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com', 'github.com'],
+  
+    },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
