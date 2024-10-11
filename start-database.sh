b752841adc5b8eea0206007fb2c59fa5d154425a
Pratham A Kadekar <pratham20442@gmail.com>
2024-10-11 19:50:37 +0530

    refactor: prisma schema for event registrations

--- start-database.sh
+++ start-database.sh
@@ -9,16 +9,21 @@
 
 # On Linux and macOS you can run this script directly - `./start-database.sh`
 
-DB_CONTAINER_NAME="csinmamit-postgres"
+DB_CONTAINER_NAME="csinmamit"
 
 if ! [ -x "$(command -v docker)" ]; then
-  echo "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
+  echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
   exit 1
 fi
 
 if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
-  docker start $DB_CONTAINER_NAME
-  echo "Database container started"
+  echo "Database container '$DB_CONTAINER_NAME' already running"
+  exit 0
+fi
+
+if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
+  docker start "$DB_CONTAINER_NAME"
+  echo "Existing database container '$DB_CONTAINER_NAME' started"
   exit 0
 fi
 
@@ -26,13 +31,14 @@ fi
 set -a
 source .env
 
-DB_PASSWORD=$(echo $DATABASE_URL | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
+DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
+DB_PORT=$(echo "$DATABASE_URL" | awk -F':' '{print $4}' | awk -F'\/' '{print $1}')
 
 if [ "$DB_PASSWORD" = "password" ]; then
   echo "You are using the default database password"
   read -p "Should we generate a random password for you? [y/N]: " -r REPLY
   if ! [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Please set a password in the .env file and try again"
+    echo "Please change the default password in the .env file and try again"
     exit 1
   fi
   # Generate a random URL-safe password
@@ -40,6 +46,14 @@ if [ "$DB_PASSWORD" = "password" ]; then
   sed -i -e "s#:password@#:$DB_PASSWORD@#" .env
 fi
 
-docker run --name $DB_CONTAINER_NAME -e POSTGRES_PASSWORD=$DB_PASSWORD -e POSTGRES_DB=csinmamit -d -p 5432:5432 docker.io/postgres
+docker run -d \
+  --name $DB_CONTAINER_NAME \
+  -e POSTGRES_USER="postgres" \
+  -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+  -e POSTGRES_DB=$DB_CONTAINER_NAME \
+  -p "$DB_PORT":5432 \
+  docker.io/postgres && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
 
-echo "Database container was successfully created"
+
+echo $DB_PASSWORD
+echo $DB_PORT
